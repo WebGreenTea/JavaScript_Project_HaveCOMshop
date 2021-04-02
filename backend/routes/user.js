4c337a4877dfa2cea5ee6c4bf59b2fbd94a8b512
sahawut <sahawut092013@gmail.com>
2021-04-02 19:58:29 +0700

    save

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,13 +2,25 @@ const express = require('express')
 const router = express.Router()
 const UserModel = require('../models/user')
 const bodyPaser = require('body-parser')
-
+var md5 = require('md5');
 
 
 // Creating one
 router.post('/register', async (req, res) => {
     console.log(req.body)
-    UserModel.create(req.body)
+    req.body.password = md5(req.body.password)
+    console.log(req.body.password)
+    
+    try{
+        const response = await UserModel.create(req.body)
+        console.log('user created successfully'+ response)
+        res.json({status: 'ok'})
+    }catch(error){
+        console.log(error)
+        return res.json({status:'error'})
+    }
+    
+    
     //res.json({status: 'ok'})
     
   })
